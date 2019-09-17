6cf102bbdbd35a398d4aa92c4b0d69d8701e007b
Mikael Jansson <gusmikaja@student.gu.se>
2019-09-17 13:06:18 +0200

    Add more functions to users controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -10,14 +10,36 @@ router.get('/', function (req, res, next) {
     });
 });
 
+//Creates a new user
 router.post('/', function (req, res, next) {
     var user = new User(req.body);
     user.save(function (err) {
         if (err) { return next(err); }
         res.status(201).json(user);
     });
-
-
 });
 
+//Returns the user with given id
+router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    User.findById(id, function(err, user) {
+        if (err) { return next(err); }
+        if (user === null) {
+            return res.status(404).json({'message': 'User not found'});
+        }
+        res.json(user);
+    });
+});
+
+//Deletes a user with the given id
+router.delete('/:id', function(req, res, next) {
+    var id = req.params.id;
+    User.findOneAndDelete({_id: id}, function(err, user) {
+        if (err) { return next(err); }
+        if (user === null) {
+            return res.status(404).json({'message': 'User not found'});
+        }
+        res.json(user);
+    });
+});
 module.exports = router;
