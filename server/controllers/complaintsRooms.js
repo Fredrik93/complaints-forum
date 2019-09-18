06688d2f6443ccac3d49fc007eebc6b22de46510
Fredrik93 <fredrikullman93@gmail.com>
2019-09-18 09:19:07 +0200

    minor fixes on complaintsRooms and achievementBoard

--- server/controllers/complaintsRooms.js
+++ server/controllers/complaintsRooms.js
@@ -3,53 +3,53 @@ var router = express.Router();
 var ComplaintsRoom = require('../models/complaintsRoom');
 
 // Return a list of all complaintsRooms
-router.get('/', function(req, res, next) {
-    ComplaintsRoom.find(function(err, complaintsRoom) {
+router.get('/', function (req, res, next) {
+    ComplaintsRoom.find(function (err, complaintsRoom) {
         if (err) { return next(err); }
-        res.json({'complaintsRooms': achievementBoard});
+        res.json({ 'complaintsRooms': complaintsRoom });
     });
 });
 
 // Create a new complaintsRooms
-router.post('/', function(req, res, next) {
-    var complaintsRoom = new AchievementBoard(req.body);
-    complaintsRoom.save(function(err) {
+router.post('/', function (req, res, next) {
+    var complaintsRoom = new ComplaintsRoom(req.body);
+    complaintsRoom.save(function (err) {
         if (err) { return next(err); }
         res.status(201).json(complaintsRoom);
     });
 });
 
 // Return the Room with the given ID
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
     var id = req.params.id;
-    ComplaintsRoom.findById(id, function(err, complaintsRoom) {
+    ComplaintsRoom.findById(id, function (err, complaintsRoom) {
         if (err) { return next(err); }
         if (complaintsRoom === null) {
-            return res.status(404).json({'message': 'Room not found'});
+            return res.status(404).json({ 'message': 'Room not found' });
         }
         res.json(complaintsRoom);
     });
 });
 
 // Delete the Rooms with the given ID
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
     var id = req.params.id;
-    ComplaintsRoom.findOneAndDelete({_id: id}, function(err, complaintsRoom) {
+    ComplaintsRoom.findOneAndDelete({ _id: id }, function (err, complaintsRoom) {
         if (err) { return next(err); }
         if (complaintsRoom === null) {
-            return res.status(404).json({'message': 'Room not found'});
+            return res.status(404).json({ 'message': 'Room not found' });
         }
         res.json(complaintsRoom);
     });
 });
 
 //Replaces a post with the given id
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function (req, res, next) {
     var id = req.params.id;
-    complaintsRoom.replaceOne({_id: id}, function(err, complaintsRoom) {
+    complaintsRoom.replaceOne({ _id: id }, function (err, complaintsRoom) {
         if (err) { return next(err); }
         if (complaintsRoom === null) {
-            return res.status(404).json({'message': 'Room not found'});
+            return res.status(404).json({ 'message': 'Room not found' });
         }
         res.json(complaintsRoom);
     });
@@ -58,15 +58,15 @@ router.put('/:id', function(req, res, next) {
 module.exports = router;
 
 //Update a complaintsrooms values by id
-router.patch('/:id', function(req, res, next) {
+router.patch('/:id', function (req, res, next) {
     var id = req.params.id;
-    ComplaintsRoom.findById(id, function(err, user) {
-        if(err) { return next(err); }
+    ComplaintsRoom.findById(id, function (err, user) {
+        if (err) { return next(err); }
         if (complaintsRoom == null) {
-            return res.status(404).json({"message": "Room not found"});
+            return res.status(404).json({ "message": "Room not found" });
         }
         complaintsRoom.maxUsers = (req.body.maxUsers || post.maxUsers);
         complaintsRoom.save();
         res.json(complaintsRoom);
-    });  
+    });
 });
