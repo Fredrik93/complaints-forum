5d36d09f826f050a9df221b7e7950de2c43c97c5
Fredrik93 <fredrikullman93@gmail.com>
2019-09-25 18:29:08 +0200

    add post

--- client/src/components/PostItem.vue
+++ client/src/components/PostItem.vue
@@ -1,7 +1,7 @@
 <template>
   <b-list-group-item :style="{ title: post.title }">
-    <img alt="Camel" src="../assets/camel.jpg" />
-    {{ post.title}} has text : {{ post.text }}
+    <img alt="Avatar" src="../assets/duckie.jpeg" />
+    {{ post.title}} wrote : {{ post.text }}
     <b-button type="button" class="close" @click="$emit('delete-camel', post._id)">&times;</b-button>
   </b-list-group-item>
 </template>
@@ -12,3 +12,9 @@ export default {
   props: ['post']
 }
 </script>
+
+<style >
+img {
+  width: 40px;
+}
+</style>
--- client/src/views/Posts.vue
+++ client/src/views/Posts.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="posts">
-    <h1>List of Posts!</h1>
-    <b-button type="button" class="createButton" @click="createCamel()">Create Camel</b-button>
+    <h1>List of {{posts.length}} posts</h1>
     <b-list-group>
-      <post-item v-for="post in posts" :key="post._id" :post="post"></post-item>
+      <post-item v-for="post in posts" :key="post._id" :post="post" @delete-post="deletePost"></post-item>
     </b-list-group>
+    <form action="/posts">
+      <input class="form-group" type="text" name="title" id="titleId" />
+      <input type="text" name="text" id="textId" />
+      <b-button type="submit" class="form-group" @click="createPost()">Create Post</b-button>
+    </form>
   </div>
 </template>
 
@@ -32,9 +36,38 @@ export default {
           this.posts = []
           console.log(error)
         })
-        .then(() => {})
+        .then(() => {
+          // This code is always executed (after success or error).
+        })
+    },
+    deletePost(id) {
+      Api.delete(`/posts/${id}`)
+        .then(response => {
+          console.log(response.data.message)
+          var index = this.posts.findIndex(post => post._id === id)
+          this.posts.splice(index, 1)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    createPost() {
+      var title = document.getElementById('titleId').value
+      var text = document.getElementById('textId').value
+      var randomPost = {
+        title: title,
+        text: text
+      }
+      Api.post('/posts', randomPost)
+        .then(response => {
+          this.posts.push(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   },
+
   components: {
     PostItem
   }
