49b265d41c08d2f9bd3c9dfaa7db241a10fb5458
Fredrik93 <fredrikullman93@gmail.com>
2019-10-03 15:40:47 +0200

    fix post

--- client/src/components/UserItem.vue
+++ client/src/components/UserItem.vue
@@ -2,20 +2,20 @@
   <b-list-group-item :style="{ color: user.username }">
     {{ user.username }}, {{ user.age }} years old
     <b-button type="button" class="close" @click="$emit('delete-user', user._id)">&times;</b-button>
-    <b-button type="button" @click="$emit('get-user', user._id)">&times;</b-button>
+    <b-button type="button" @click="$emit('create-post', user._id)">&times;</b-button>
   </b-list-group-item>
 </template>
 
 <script>
-import UserItem from "@/components/UserItem";
+import UserItem from '@/components/UserItem';
 
 export default {
-  name: "user-item",
-  props: ["user"]
-};
+  name: 'user-item',
+  props: ['user']
+}
 
 components: {
-  UserItem;
+  UserItem
 }
 </script>
 
